290f979a7741739256542e7d2956afe72f53ec66
Andrew Hollis <thejester129@gmail.com>
2021-06-06 20:51:09 +0100

    Added some quotes

--- app/src/main/java/com/example/reptimer/sound/RepMediaPlayer.kt
+++ app/src/main/java/com/example/reptimer/sound/RepMediaPlayer.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.media.MediaPlayer
 import android.net.Uri
 import com.example.reptimer.R
+import kotlin.random.Random
 
 
 class RepMediaPlayer(private val context: Context) : RepSoundPlayer {
@@ -13,12 +14,24 @@ class RepMediaPlayer(private val context: Context) : RepSoundPlayer {
     }
 
     override fun playRepSound() {
-        val assetFileDescriptor = context.resources.openRawResourceFd(R.raw.stay_hard_1) ?: return
+        val assetFileDescriptor = context.resources.openRawResourceFd(getRandomSoundbite()) ?: return
         mediaPlayer.run {
             reset()
             setDataSource(assetFileDescriptor.fileDescriptor, assetFileDescriptor.startOffset, assetFileDescriptor.declaredLength)
             prepareAsync()
         }
     }
+
+    private fun getRandomSoundbite():Int{
+        val index = Random.nextInt(soundClips.size)
+        return soundClips[index]
+    }
+
+    private val soundClips = arrayOf(R.raw.stay_hard_1,
+                                     R.raw.stay_hard_2,
+                                     R.raw.stay_hard_2,
+
+                                    )
+
 
 }
